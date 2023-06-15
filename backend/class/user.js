99ad9811ee84e86afcc18971119710acb25ca75a
velpoopy <bryanttanyq98@gmail.com>
2023-06-15 22:38:13 +0800

    Added back signup function

--- backend/class/user.js
+++ backend/class/user.js
@@ -1,18 +1,47 @@
 class User {
-    constructor(userID, username, password, email, mobileNumber, receiveNotification) {
-      this.userID = userID;
-      this.username = username;
-      this.password = password;
-      this.email = email;
-      this.mobileNumber = mobileNumber;
-      this.receiveNotification = receiveNotification;
-    }
-  
-    login() {
-      console.log("User logged in");
-    }
-  
-    logout() {
-      console.log("User logged out");
+  constructor(client, userID, username, password, email, mobileNumber, receiveNotification) {
+    this.client = client;
+    this.userID = userID;
+    this.username = username;
+    this.password = password;
+    this.email = email;
+    this.mobileNumber = mobileNumber;
+    this.receiveNotification = receiveNotification;
+  }
+
+  async login(res, username, password) {
+    try {
+      const db = this.client.db('FreshBearNearMe');
+      const collection = db.collection('User');
+      //Find a document that matches the username and password
+      const result = await collection.findOne({ username, password });
+
+      if (result) {
+        //Create a session token
+        const sessionToken = 'testtoken123';
+
+        //Set the session token in a cookie
+        res.cookie('sessionToken', sessionToken, { httpOnly: true });
+
+        //Set the username in a separate cookie
+        res.cookie('username', result.username, { httpOnly: true });
+
+        //Set object id in a separate cookie
+        res.cookie('id', result.id, { httpOnlu: true});
+        
+        res.json({ success: true, username: result.username, password: result.password });
+      } else {
+        res.json({ success: false, message: 'Invalid username or password' });
+      }
+    } catch (error) {
+      console.error('Error during login:', error);
+      res.status(500).json({ success: false, message: 'An error occurred during login' });
     }
   }
+
+  logout() {
+    console.log("User logged out");
+  }
+}
+
+module.exports = User;
--- backend/server.js
+++ backend/server.js
@@ -5,8 +5,10 @@ const port = 3000;
 const { MongoClient } = require('mongodb');
 const uri = 'mongodb+srv://Admin:admin123@fyp.qzlvrug.mongodb.net/?retryWrites=true&w=majority';
 const client = new MongoClient(uri);
+const db = client.db('FreshBearNearMe');
+const collection = db.collection('User');
 
-// Connect to MongoDB
+//Connect to MongoDB
 async function connectToMongoDB() {
   try {
     await client.connect();
@@ -16,16 +18,37 @@ async function connectToMongoDB() {
   }
 }
 
-// Call the MongoDB connection function
+//Call the MongoDB connection function
 connectToMongoDB();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-// Route to verify username and password
+//Route to verify username and password
 app.post('/', async (req, res) => {
   const { username, password } = req.body;
   const user = new User(client, "", username, password, "", "", false);
   await user.login(res, username, password);
+});
+
+//Route to signup user
+app.post('/signup', async (req, res) => {
+  const { username, password, email, mobileNumber, receiveNotification } = req.body;
+
+  try {
+    //Insert the user data into the MongoDB collection
+    const result = await collection.insertOne({
+      username,
+      password,
+      email,
+      mobileNumber,
+      receiveNotification: false
+    });
+
+    res.json({ success: true, message: 'User signed up successfully' });
+  } catch (error) {
+    console.error('Error during signup:', error);
+    res.status(500).json({ success: false, message: 'An error occurred during signup' });
+  }
 });
 
 app.get('/', (req, res) => {
