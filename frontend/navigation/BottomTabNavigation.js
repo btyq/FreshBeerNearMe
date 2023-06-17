0c4aa33e214bf0ecd9b9497e87b2fb458d249507
Celene <celene.xy@gmail.com>
2023-06-17 19:15:43 +0800

    update bottomtabnavigation

--- frontend/navigation/BottomTabNavigation.js
+++ frontend/navigation/BottomTabNavigation.js
@@ -1,9 +1,9 @@
 import { View, Text, Platform } from 'react-native'
 import React from 'react'
-import { Feather, Ionicons, FontAwesome, FontAwesome5, Entypo, AntDesign } from '@expo/vector-icons'
+import { Feather, Ionicons, FontAwesome, FontAwesome5, Entypo, AntDesign, MaterialIcons } from '@expo/vector-icons'
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs'
 import  COLORS from '../constants/colors'
-import { Dashboard, Profile, BeersVenue, Welcome } from '../screens'
+import { Dashboard, Profile, BeersVenue, Welcome, Social, Feedback, Journal } from '../screens'
 
 const Tab = createBottomTabNavigator()
 
@@ -74,6 +74,51 @@ const BottomTabNavigation = () => {
                 }}
             />
 
+            <Tab.Screen
+                name="Social"
+                component={Social}
+                options={{
+                    tabBarIcon: ({ focused }) => {
+                        return (
+                            <Ionicons
+                                name="people"
+                                size={24}
+                                color={COLORS.black}
+                            />
+                        )
+                    },
+                }}
+            />
+            <Tab.Screen
+                name="Feedback"
+                component={Feedback}
+                options={{
+                    tabBarIcon: ({ focused }) => {
+                        return (
+                            <MaterialIcons
+                                name="feedback"
+                                size={24}
+                                color={COLORS.black}
+                            />
+                        )
+                    },
+                }}
+            />
+            <Tab.Screen
+                name="Journal"
+                component={Journal}
+                options={{
+                    tabBarIcon: ({ focused }) => {
+                        return (
+                            <Ionicons
+                                name="journal"
+                                size={24}
+                                color={COLORS.black}
+                            />
+                        )
+                    },
+                }}
+            />
             <Tab.Screen
                 name="Welcome"
                 component={Welcome}
