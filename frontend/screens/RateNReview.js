17b70e9259e26d3120d35d3401484d6230bce7d7
gcjk768 <james.kohgc@gmail..com>
2023-06-28 02:42:37 +0800

    Update RateNReview Page

--- frontend/screens/RateNReview.js
+++ frontend/screens/RateNReview.js
@@ -43,7 +43,7 @@ const PopOut = (props) => {
   );
 };
 
-const Social = () => {
+const RateNReview = () => {
   const navigation = useNavigation();
   const [comment, setComment] = useState('Bitter, but it\'s decent');
   const [comments, setComments] = useState([]);
@@ -66,7 +66,7 @@ const Social = () => {
   const navigateToSocial = () => {
     // Navigate to the Forums.js page
     navigation.navigate('Social');
-  }; 
+  };
 
   const navigateToForums = () => {
     // Navigate to the Forums.js page
@@ -90,11 +90,24 @@ const Social = () => {
 
   return (
     <ScrollView>
-      <LinearGradient style={{ height: 1500 }} colors={[COLORS.white, COLORS.yellow]}>
+      <View style={{ height: 1500, backgroundColor: COLORS.white }}>
         <Header
           placement="left"
-          backgroundColor={COLORS.foam}
-          centerComponent={{ text: 'FreshBeer', style: { fontSize: 20, color: COLORS.black, fontWeight: 'bold', flexDirection: 'row' } }}
+          backgroundColor={COLORS.primary}
+          containerStyle={{
+            height: 100,
+            borderBottomLeftRadius: 40,
+            borderBottomRightRadius: 40,
+          }}
+          centerComponent={{
+            text: 'FreshBeer',
+            style: {
+              fontSize: 20,
+              color: COLORS.black,
+              fontWeight: 'bold',
+              flexDirection: 'row',
+            },
+          }}
           rightComponent={
             <View style={{ flexDirection: 'row', marginTop: 5 }}>
               <TouchableOpacity>
@@ -103,9 +116,9 @@ const Social = () => {
               <TouchableOpacity>
                 <Ionicons name="notifications-outline" size={24} color={COLORS.black} />
               </TouchableOpacity>
-            </View>}
+            </View>
+          }
         />
-
         <SafeAreaView style={{ flex: 1 }}>
           <View style={styles.grid}>
             <Button
@@ -115,7 +128,7 @@ const Social = () => {
               style={styles.longButton}
               onPress={navigateToSocial}
             />
-           <Button
+            <Button
               title="Forums"
               color={COLORS.white}
               filled
@@ -124,7 +137,7 @@ const Social = () => {
             />
             <Button
               title="Rate & Review"
-              color={COLORS.foam}
+              color={COLORS.grey}
               filled
               style={styles.longButton}
               onPress={navigateToRateNReview}
@@ -151,23 +164,222 @@ const Social = () => {
               style={styles.searchInput}
             />
             <Button
-              title="Search for user"
-              color={COLORS.foam}
+              title="Search"
+              color={COLORS.grey}
               filled
               style={styles.searchButton}
             />
           </View>
+
+          <TouchableOpacity
+            style={{
+              backgroundColor: COLORS.white,
+              width: '90%',
+              height: '3%',
+              borderWidth: 1,
+              borderColor: COLORS.grey,
+              borderRadius: 10,
+              marginTop: 10,
+              alignSelf: 'center',
+              ...styles.containerContent,
+            }}
+            onPress={showPopOut}
+          >
+            <Text
+              style={{
+                fontSize: 16,
+                fontWeight: 'bold',
+                marginLeft: 10,
+                marginTop: 6,
+                ...styles.beerName,
+              }}
+            >
+              Beer Name
+            </Text>
+            <View
+              style={{
+                position: 'relative',
+                top: -13,
+                marginLeft: 'auto',
+                marginRight: 0,
+                ...styles.starContainer,
+              }}
+            >
+              {/* Add Airbnb star component here */}
+              <AirbnbRating
+                count={5}
+                defaultRating={4}
+                showRating={false}
+                size={20}
+                starContainerStyle={{
+                  marginLeft: -8, // Adjust the value to bring the stars closer
+                  marginTop: -5,
+                  ...styles.ratingStarContainer,
+                }}
+                starStyle={styles.ratingStyle}
+                isDisabled={true} // Make the star rating read-only
+              />
+            </View>
+          </TouchableOpacity>
+          <PopOut visible={popOutVisible} text="This is a pop-up" onPress={closePopOut} />
+          <Button
+            title="Write a Review"
+            color={COLORS.primary}
+            filled
+            onPress={() => {
+              // Handle button press
+            }}
+            style={{
+              ...styles.rateButton,
+              backgroundColor: COLORS.grey, // Set the desired background color
+              borderColor: COLORS.grey, // Set the desired border color
+              borderWidth: 1, // Set the desired border thickness
+              marginTop: 0,
+              marginRight: 20,
+            }}
+          />
+          <TouchableOpacity
+            style={{
+              backgroundColor: COLORS.white,
+              width: '90%',
+              height: '3%',
+              borderWidth: 1,
+              borderColor: COLORS.grey,
+              borderRadius: 10,
+              marginTop: 30,
+              alignSelf: 'center',
+              ...styles.containerContent,
+            }}
+            onPress={showPopOut}
+          >
+            <Text
+              style={{
+                fontSize: 16,
+                fontWeight: 'bold',
+                marginLeft: 10,
+                marginTop: 6,
+                ...styles.beerName,
+              }}
+            >
+              Beer Name
+            </Text>
+            <View
+              style={{
+                position: 'relative',
+                top: -13,
+                marginLeft: 'auto',
+                marginRight: 0,
+                ...styles.starContainer,
+              }}
+            >
+              {/* Add Airbnb star component here */}
+              <AirbnbRating
+                count={5}
+                defaultRating={4}
+                showRating={false}
+                size={20}
+                starContainerStyle={{
+                  marginLeft: -8, // Adjust the value to bring the stars closer
+                  marginTop: -5,
+                  ...styles.ratingStarContainer,
+                }}
+                starStyle={styles.ratingStyle}
+                isDisabled={true} // Make the star rating read-only
+              />
+            </View>
+          </TouchableOpacity>
+          <PopOut visible={popOutVisible} text="This is a pop-up" onPress={closePopOut} />
+          <Button
+            title="Write a Review"
+            color={COLORS.primary}
+            filled
+            onPress={() => {
+              // Handle button press
+            }}
+            style={{
+              ...styles.rateButton,
+              backgroundColor: COLORS.grey, // Set the desired background color
+              borderColor: COLORS.grey, // Set the desired border color
+              borderWidth: 1, // Set the desired border thickness
+              marginTop: 0,
+              marginRight: 20,
+            }}
+          />
+          <TouchableOpacity
+            style={{
+              backgroundColor: COLORS.white,
+              width: '90%',
+              height: '3%',
+              borderWidth: 1,
+              borderColor: COLORS.grey,
+              borderRadius: 10,
+              marginTop: 30,
+              alignSelf: 'center',
+              ...styles.containerContent,
+            }}
+            onPress={showPopOut}
+          >
+            <Text
+              style={{
+                fontSize: 16,
+                fontWeight: 'bold',
+                marginLeft: 10,
+                marginTop: 6,
+                ...styles.beerName,
+              }}
+            >
+              Venue Name
+            </Text>
+            <View
+              style={{
+                position: 'relative',
+                top: -13,
+                marginLeft: 'auto',
+                marginRight: 0,
+                ...styles.starContainer,
+              }}
+            >
+              {/* Add Airbnb star component here */}
+              <AirbnbRating
+                count={5}
+                defaultRating={4}
+                showRating={false}
+                size={20}
+                starContainerStyle={{
+                  marginLeft: -8, // Adjust the value to bring the stars closer
+                  marginTop: -5,
+                  ...styles.ratingStarContainer,
+                }}
+                starStyle={styles.ratingStyle}
+                isDisabled={true} // Make the star rating read-only
+              />
+            </View>
+          </TouchableOpacity>
+          <PopOut visible={popOutVisible} text="This is a pop-up" onPress={closePopOut} />
+          <Button
+            title="Write a Review"
+            color={COLORS.primary}
+            filled
+            onPress={() => {
+              // Handle button press
+            }}
+            style={{
+              ...styles.rateButton,
+              backgroundColor: COLORS.grey, // Set the desired background color
+              borderColor: COLORS.grey, // Set the desired border color
+              borderWidth: 1, // Set the desired border thickness
+              marginTop: 0,
+              marginRight: 20,
+            }}
+          />
+
         </SafeAreaView>
-      </LinearGradient>
+      </View>
     </ScrollView>
   );
 };
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: COLORS.foam,
-  },
   topBar: {
     flexDirection: 'row',
     justifyContent: 'space-between',
@@ -204,8 +416,8 @@ const styles = StyleSheet.create({
   },
   button: {
     paddingVertical: 10,
-    borderColor: COLORS.black,
-    borderWidth: 2,
+    borderColor: COLORS.grey,
+    borderWidth: 1,
     borderRadius: 12,
     alignItems: 'center',
     justifyContent: 'center',
@@ -218,7 +430,7 @@ const styles = StyleSheet.create({
   },
   searchInput: {
     height: 40,
-    borderColor: 'gray',
+    borderColor: 'grey',
     borderWidth: 1,
     borderRadius: 10,
     paddingLeft: 10,
@@ -229,13 +441,15 @@ const styles = StyleSheet.create({
     width: '30%',
     height: 40,
     borderRadius: 30,
+    borderWidth: 1,
+    borderColor: COLORS.grey,
   },
   userContainer: {
     padding: 10,
     margin: 10,
     backgroundColor: COLORS.white,
     borderColor: COLORS.black,
-    borderWidth: 2,
+    borderWidth: 1,
     borderRadius: 10,
     shadowColor: COLORS.black,
     shadowOffset: { width: 0, height: 2 },
@@ -302,38 +516,47 @@ const styles = StyleSheet.create({
   ratingStyle: {
     position: 'relative',
   },
-  popOutContainer: {
-    position: 'absolute',
-    top: 0,
-    left: 0,
-    right: 0,
-    bottom: 0,
+  rateButton: {
+    marginLeft: 'auto',
+    marginRight: 10,
+  },
+  containerContent: {
+    marginBottom: 20,
+  },
+  beerName: {
+    marginBottom: 5,
+  },
+  starContainer: {
+    flexDirection: 'row',
     alignItems: 'center',
-    justifyContent: 'center',
+  },
+  popOutContainer: {
+    flex: 1,
     backgroundColor: 'rgba(0, 0, 0, 0.5)',
-    zIndex: 999,
+    justifyContent: 'center',
+    alignItems: 'center',
   },
   popOutContent: {
     backgroundColor: COLORS.white,
     padding: 20,
     borderRadius: 10,
-    alignItems: 'center',
+    width: '80%',
   },
   popOutText: {
-    fontSize: 18,
+    fontSize: 16,
     marginBottom: 20,
-    textAlign: 'center',
   },
   popOutButton: {
     backgroundColor: COLORS.primary,
-    paddingVertical: 10,
-    paddingHorizontal: 20,
+    padding: 10,
     borderRadius: 10,
+    alignItems: 'center',
   },
   popOutButtonText: {
     color: COLORS.white,
-    fontSize: 16,
+    fontSize: 14,
+    fontWeight: 'bold',
   },
 });
 
-export default Social;
+export default RateNReview;
