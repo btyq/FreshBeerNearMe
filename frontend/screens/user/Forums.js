99ab1af490d345d583d3aa282792cbaae56216ef
Celene <celene.xy@gmail.com>
2023-07-22 00:13:21 +0800

    added forums page popup

--- frontend/screens/user/Forums.js
+++ frontend/screens/user/Forums.js
@@ -53,46 +53,69 @@ const CustomText = (props) => {
 };
 
 const Posts = () => {
+	const [popupVisible, setPopupVisible] = useState(false); // 1st popup
+	const [popupVisible2, setPopupVisible2] = useState(false); // 2nd popup for comments
+	const [comment, setcomment] = useState(null);
+
+	const handlePopUp = (comment) => {
+		setcomment(comment);
+		setPopupVisible(!popupVisible);
+	};
+
+	const handlePopUp2 = () => {
+		setPopupVisible2(!popupVisible2);
+	};
+
 	const comments = [
 		{
 			text: "Bitter, but it's decent",
 			image: require("../../assets/beer.png"),
+			description: "Bitter like my life",
 		},
 		{
 			text: "Smooth and refreshing",
 			image: require("../../assets/beer1.png"),
+			description: "Smooth like butter",
 		},
 		{
 			text: "Great aroma and flavor",
 			image: require("../../assets/beer3.png"),
+			description: "Nice smell i guess",
 		},
 		{
 			text: "Not my favorite, too hoppy",
 			image: require("../../assets/beer.png"),
+			description: "Ya so I don't like beer",
 		},
 		{
 			text: "Love the rich maltiness",
 			image: require("../../assets/beer1.png"),
+			description: "Rich is good",
 		},
 		{
 			text: "Too bitter for my taste",
 			image: require("../../assets/beer3.png"),
+			description: "Very bitter, very salty",
 		},
 		{
 			text: "Lacks complexity",
 			image: require("../../assets/beer.png"),
+			description: "Not smooth like butter already",
 		},
 		{
 			text: "Delicious and well-balanced",
 			image: require("../../assets/beer1.png"),
+			description: "I can't even balance myself",
 		},
 		{
 			text: "A bit watery, but still enjoyable",
 			image: require("../../assets/beer3.png"),
+			description: "Watery is bad",
 		},
 		{
 			text: "Strong and full-bodied",
 			image: require("../../assets/beer.png"),
+			description: "Wow stronk stronk",
 		},
 	];
 
@@ -101,13 +124,10 @@ const Posts = () => {
 			showsVerticalScrollIndicator={false}
 			contentContainerStyle={{ flexGrow: 1, paddingBottom: 30 }}
 		>
-			<SafeAreaView
-				style={{
-					marginHorizontal: 20,
-				}}
-			>
+			<SafeAreaView style={{ marginHorizontal: 20 }}>
 				{comments.map((comment, index) => (
-					<View
+					<TouchableOpacity
+						onPress={() => handlePopUp(comment)}
 						key={index}
 						style={{
 							backgroundColor: COLORS.grey,
@@ -115,9 +135,8 @@ const Posts = () => {
 							borderColor: 0,
 							borderRadius: 15,
 							padding: 10,
-							marginBottom: 20,
+							marginBottom: 5,
 						}}
-						// onPress={() => handleSubContainerPress(index)}
 					>
 						<View
 							style={{
@@ -146,8 +165,128 @@ const Posts = () => {
 							</Text>
 						</View>
 						<CustomText style={{ marginTop: 22 }}>Posted by:</CustomText>
-					</View>
+					</TouchableOpacity>
 				))}
+
+				{/* for popup */}
+				<Modal visible={popupVisible} transparent animationType="fade">
+					{/* <View> */}
+					<View
+						style={{
+							width: "100%",
+							height: "100%",
+							backgroundColor: COLORS.secondary,
+							borderRadius: 10,
+							paddingHorizontal: 20,
+							elevation: 5,
+						}}
+					>
+						<TouchableOpacity onPress={handlePopUp}>
+							<Ionicons
+								name="arrow-back"
+								size={24}
+								color={COLORS.black}
+								style={{ marginTop: 12 }}
+							/>
+						</TouchableOpacity>
+						<View
+							style={{
+								flexDirection: "row",
+								alignItems: "center",
+								marginVertical: 20,
+							}}
+						>
+							<Image
+								source={require("../../assets/beer.png")}
+								style={{
+									width: 60,
+									height: 60,
+									borderRadius: 80,
+									alignSelf: "flex-start",
+								}}
+								resizeMode="contain"
+							/>
+							<View style={{ flexDirection: "column", marginLeft: 15 }}>
+								<Text style={{ ...GlobalStyle.headerFont, fontSize: 15 }}>
+									username
+								</Text>
+								<CustomText style={{ fontSize: 12 }}>1 day ago</CustomText>
+							</View>
+						</View>
+						{comment ? (
+							<View>
+								<Text style={{ ...GlobalStyle.headerFont, fontSize: 18 }}>
+									{comment.text}
+								</Text>
+								<CustomText>{comment.description}</CustomText>
+							</View>
+						) : (
+							<CustomText>No comment selected</CustomText>
+						)}
+						<Button
+							title="+ Add a comment"
+							onPress={handlePopUp2}
+							filled
+							style={{
+								elevation: 2,
+								borderColor: 0,
+								marginTop: 20,
+							}}
+						/>
+						{/* 2nd popup */}
+						<Modal visible={popupVisible2} transparent animationType="slide">
+							<View
+								style={{
+									flex: 1,
+									backgroundColor: COLORS.overlay,
+									justifyContent: "flex-end",
+									alignItems: "center",
+								}}
+							>
+								<View
+									style={{
+										backgroundColor: COLORS.secondary,
+										width: "100%",
+										padding: 20,
+										borderColor: COLORS.black,
+										borderWidth: 1,
+										borderTopLeftRadius: 20,
+										borderTopRightRadius: 20,
+									}}
+								>
+									<View
+										style={{
+											height: 45,
+											borderColor: 0,
+											borderWidth: 1,
+											borderRadius: 10,
+											alignItems: "center",
+											justifyContent: "center",
+											paddingLeft: 22,
+											marginVertical: 20,
+											backgroundColor: COLORS.grey,
+										}}
+									>
+										<TextInput
+											placeholder="Type a comment here"
+											keyboardType="default"
+										/>
+									</View>
+									<Button
+										title="Submit"
+										onPress={handlePopUp2}
+										filled
+										style={{
+											elevation: 2,
+											borderColor: 0,
+										}}
+									/>
+								</View>
+							</View>
+						</Modal>
+					</View>
+					{/* </View> */}
+				</Modal>
 			</SafeAreaView>
 		</ScrollView>
 	);
@@ -267,6 +406,7 @@ const Forum = ({ navigation }) => {
 							// onPress={navigateToNewPost}
 						/>
 					</View>
+
 					<Posts />
 				</SafeAreaView>
 			</SafeAreaView>
